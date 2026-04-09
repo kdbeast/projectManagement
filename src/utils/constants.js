a41933198d3c6909269bb2eb046e52cb1ecb0e24
Karan Jamwal <karanjamwal2004@gmail.com>
2026-04-09 21:20:06 +0530

    feat: add ApiError, ApiResponse classes and constants for user roles and task statuses

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+
+export const AvailableRolesEnum = Object.values(UserRolesEnum);
+
+export const TaskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+export const AvailableTaskStatusEnum = Object.values(TaskStatusEnum);
