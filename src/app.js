7063f1aa30c7729c862b4933c3e8b04ea4cf8497
Karan Jamwal <karanjamwal2004@gmail.com>
2026-04-09 21:00:28 +0530

    feat: initialize Express application with CORS and basic configuration
    - Added app.js to set up Express server with JSON and URL-encoded body parsing
    - Configured CORS with dynamic origin and allowed methods/headers
    - Created index.js to start the server and load environment variables
    - Added .gitkeep file in public/images directory to track empty folder

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to basecampy!");
+});
+
+export default app;
