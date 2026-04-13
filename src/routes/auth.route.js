ec59d08f88ab3b2f3675da964c2303e59fc975b3
Karan Jamwal <karanjamwal2004@gmail.com>
2026-04-13 14:28:39 +0530

    feat: add password reset and email verification validation logic

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,21 +2,53 @@ import { Router } from "express";
 import {
   userLoginValidation,
   userRegistorValidation,
+  userResetPasswordValidation,
+  userForgetPasswordValidation,
 } from "../validators/index.js";
 import {
   loginUser,
-  registerUser,
   logoutUser,
+  verifyEmail,
+  registerUser,
+  refreshToken,
+  resetPassword,
+  getCurrentUser,
+  forgotPassword,
+  changeCurrentPassword,
+  resendVerificationEmail,
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
+// unsecured routes
 router
   .route("/register")
   .post(userRegistorValidation(), validate, registerUser);
 router.route("/login").post(userLoginValidation(), validate, loginUser);
+router.route("/verify-email/:token").get(verifyEmail);
+router.route("/refresh-token").get(refreshToken);
+router
+  .route("/forget-password")
+  .post(userForgetPasswordValidation(), validate, forgotPassword);
+router
+  .route("/reset-password/:token")
+  .post(userResetPasswordValidation(), validate, resetPassword);
+
+// secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userResetPasswordValidation(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/resend-email-verification")
+  .get(verifyJWT, resendVerificationEmail);
 
 export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -24,6 +24,7 @@ const userRegistorValidation = (req, res, next) => {
     body("fullName").trim().optional(),
   ];
 };
+
 const userLoginValidation = (req, res, next) => {
   return [
     body("email")
@@ -36,4 +37,47 @@ const userLoginValidation = (req, res, next) => {
   ];
 };
 
-export { userRegistorValidation, userLoginValidation };
+const userChangePasswordValidation = (req, res, next) => {
+  return [
+    body("currentPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Current password is required"),
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("New password is required")
+      .isLength({ min: 6 })
+      .withMessage("New password must be at least 6 characters long"),
+  ];
+};
+
+const userForgetPasswordValidation = (req, res, next) => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format"),
+  ];
+};
+
+const userResetPasswordValidation = (req, res, next) => {
+  return [
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("New password is required")
+      .isLength({ min: 6 })
+      .withMessage("New password must be at least 6 characters long"),
+  ];
+};
+
+export {
+  userLoginValidation,
+  userRegistorValidation,
+  userResetPasswordValidation,
+  userForgetPasswordValidation,
+  userChangePasswordValidation,
+};
